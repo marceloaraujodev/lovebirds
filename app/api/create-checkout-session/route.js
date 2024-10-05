73ba7991752913a4d82751161a721ad3c6a00851
Marcelo Araujo <codetrsh@gmail.com>
2024-10-05 19:42:41 -0400

    server res

--- app/api/create-checkout-session/route.js
+++ app/api/create-checkout-session/route.js
@@ -21,88 +21,87 @@ if (!fs.existsSync(TEMP_UPLOAD_DIR)) {
 }
 
 export async function POST(req, res){
-  // try {
-  //   await mongooseConnect();
-  //   // const {name, date, time, url, hash, photos, message} = await req.json();
-  //   // console.log('this is url:', url);
+  try {
+    await mongooseConnect();
+    // const {name, date, time, url, hash, photos, message} = await req.json();
+    // console.log('this is url:', url);
 
-  //    // Read the raw body from the request
-  //    const formData = await req.formData();
+     // Read the raw body from the request
+     const formData = await req.formData();
 
-  //    // Extract data from formData
-  //    const name = formData.get('name');
-  //    const date = formData.get('date');
-  //    const time = formData.get('time');
-  //    const musicLink = formData.get('musicLink');
-  //    const url = formData.get('url');
-  //    const hash = formData.get('hash');
-  //    const message = formData.get('message');
+     // Extract data from formData
+     const name = formData.get('name');
+     const date = formData.get('date');
+     const time = formData.get('time');
+     const musicLink = formData.get('musicLink');
+     const url = formData.get('url');
+     const hash = formData.get('hash');
+     const message = formData.get('message');
 
-  //    const photos = [];
-  //    const photoFiles = formData.getAll('photos');
+     const photos = [];
+     const photoFiles = formData.getAll('photos');
      
-  //     // Store uploaded files temporarily
-  //     for (const file of photoFiles) {
-  //       const filePath = path.join(TEMP_UPLOAD_DIR, file.name); // Create a unique file path
-  //       const fileBuffer = await file.arrayBuffer(); // Convert File to ArrayBuffer
-  //       fs.writeFileSync(filePath, Buffer.from(fileBuffer)); // Save the file to the temporary directory
-  //       photos.push(file.name); // Store the name for reference later
-  //     }
+      // Store uploaded files temporarily
+      for (const file of photoFiles) {
+        const filePath = path.join(TEMP_UPLOAD_DIR, file.name); // Create a unique file path
+        const fileBuffer = await file.arrayBuffer(); // Convert File to ArrayBuffer
+        fs.writeFileSync(filePath, Buffer.from(fileBuffer)); // Save the file to the temporary directory
+        photos.push(file.name); // Store the name for reference later
+      }
 
-  //   console.log(JSON.stringify(photos));
-  //    console.log('------------',{name, photos, date, time, url, hash, message, musicLink})
+    console.log(JSON.stringify(photos));
+     console.log('------------',{name, photos, date, time, url, hash, message, musicLink})
   
 
-  //   // // send qrcode to user email
-  //   // const transporter = nodemailer.createTransport({
-  //   //   service: 'gmail',
-  //   //   auth: {
-  //   //     user: process.env.GMAIL_USER,
-  //   //     pass: process.env.GMAIL_PASSWORD
-  //   //   }
-  //   // });
+    // // send qrcode to user email
+    // const transporter = nodemailer.createTransport({
+    //   service: 'gmail',
+    //   auth: {
+    //     user: process.env.GMAIL_USER,
+    //     pass: process.env.GMAIL_PASSWORD
+    //   }
+    // });
 
-  //   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-  //   const session = await stripe.checkout.sessions.create({
-  //     // payment_method_types: ['pix', 'card'],
-  //     line_items: [
-  //       {
-  //         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-  //         // price: 'price_1Q4lIzBfcEidHzvrx2TlCEUc', // price test item
-  //         price: 'price_1Q6fytBfcEidHzvrGWA63Iux', // live
-  //         quantity: 1,
-  //       },
-  //     ],
-  //     mode: 'payment',
-  //     success_url: `https://www.qrcodelove.com/api/create-checkout-sessiony/${url}`,
-  //     cancel_url: `${siteUrl}/error`,
-  //     metadata: {
-  //       name, 
-  //       date, 
-  //       time, 
-  //       url, 
-  //       hash, 
-  //       photos, 
-  //       musicLink,
-  //       message, 
-  //       photos: JSON.stringify(photos), // Store file names
-  //        // Pass relevant data as metadata
-  //     }
-  //   });
+    const session = await stripe.checkout.sessions.create({
+      // payment_method_types: ['pix', 'card'],
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          // price: 'price_1Q4lIzBfcEidHzvrx2TlCEUc', // price test item
+          price: 'price_1Q6fytBfcEidHzvrGWA63Iux', // live
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `https://www.qrcodelove.com/api/create-checkout-sessiony/${url}`,
+      cancel_url: `${siteUrl}/error`,
+      metadata: {
+        name, 
+        date, 
+        time, 
+        url, 
+        hash, 
+        photos, 
+        musicLink,
+        message, 
+        photos: JSON.stringify(photos), // Store file names
+         // Pass relevant data as metadata
+      }
+    });
 
-  //   // console.log('before response')
-  //   return NextResponse.json({
-  //     message: 'success',
-  //     url: session.url,
-  //   })
+    // console.log('before response')
+    return NextResponse.json({
+      message: 'success',
+      url: session.url,
+    })
     
-  // } catch (error) {
-  //   console.log(error)
-  //   return NextResponse.json({ message: 'error', error })
-  // }
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ message: 'error', error })
+  }
 
-  return NextResponse.json({ success: true})
 }
 
 
