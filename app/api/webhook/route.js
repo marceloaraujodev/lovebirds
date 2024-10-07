aa6a890d427d926fd86d15fece3672f3f7f5024c
Marcelo Araujo <codetrsh@gmail.com>
2024-10-07 16:41:32 -0400

    webhook

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -42,99 +42,98 @@ console.log(siteUrl)
 const stripe = new Stripe(process.env.STRIPE_LIVE_SECRET_KEY);
 const endpointSecret = process.env.STRIPE_LIVE_WEBHOOK_ENDPOINT_SECRET;
 
-// console.log('ENDPOINT SECRET: ', endpointSecret);
 
 export async function POST (req) {
-  // console.log('-----------------WEBHOOK HANDLER ----------------')
-  // // CHECKS IF ENDPOINTSECRET ITS WORKING
-  // if (!endpointSecret) {
-  //   return NextResponse.json({ error: 'Webhook ENDPOINTSECRET is not configured' }, { status: 500 });
-  // }
+  console.log('-----------------WEBHOOK HANDLER ----------------')
+  // CHECKS IF ENDPOINTSECRET ITS WORKING
+  if (!endpointSecret) {
+    return NextResponse.json({ error: 'Webhook ENDPOINTSECRET is not configured' }, { status: 500 });
+  }
 
-  // await mongooseConnect();
-  // const sig = req.headers.get('stripe-signature');
-  // const rawBody = await req.text();
-  // // console.log('Raw body:', rawBody.toString());
+  await mongooseConnect();
+  const sig = req.headers.get('stripe-signature');
+  const rawBody = await req.text();
+  // console.log('Raw body:', rawBody.toString());
   
-  // let event;
+  let event;
 
-  // // contructs stripe event 
-  // try { 
-  //   console.log('enter try block')
-  //   event = stripe.webhooks.constructEvent(rawBody, sig, endpointSecret);
-  //   // console.log('event', event)
-  // } catch (err) {
-  //   console.log('there was an error', err.message)
-  //   return NextResponse.json({ error: `Webhook Error: ${err.message}` }, { status: 400 });
-  // }
+  // contructs stripe event 
+  try { 
+    console.log('enter try block')
+    event = stripe.webhooks.constructEvent(rawBody, sig, endpointSecret);
+    // console.log('event', event)
+  } catch (err) {
+    console.log('there was an error', err.message)
+    return NextResponse.json({ error: `Webhook Error: ${err.message}` }, { status: 400 });
+  }
 
-  // console.log('AFTER EVENT TRY BLOCK')
+  console.log('AFTER EVENT TRY BLOCK')
   
   
-  // if (event.type === 'checkout.session.completed') {
-  //   console.log('ENTER CHECKOUT.SESSION.COMPLIETED')
-  //   const data = event.data.object;
-  //   const paid = data.payment_status === 'paid';
+  if (event.type === 'checkout.session.completed') {
+    console.log('ENTER CHECKOUT.SESSION.COMPLIETED')
+    const data = event.data.object;
+    const paid = data.payment_status === 'paid';
 
-  //   console.log('email ------>>>>>>>>>>>>>',data.customer_details.email)
-  //   const customerEmail = data.customer_details.email
+    console.log('email ------>>>>>>>>>>>>>',data.customer_details.email)
+    const customerEmail = data.customer_details.email
 
-  //   // Retrieve metadata from session
-  //   const { name, date, time, url, hash } = data.metadata;
+    // Retrieve metadata from session
+    const { name, date, time, url, hash } = data.metadata;
  
-  //   if (paid) {
+    if (paid) {
 
-  //     try {
-  //       // Generate QR code
-  //       const qrcode = await generateQRCode(`${siteUrl}/${url}`);
+      try {
+        // Generate QR code
+        const qrcode = await generateQRCode(`${siteUrl}/${url}`);
         
-  //       // Sends qr code url to email, hash is used to add qrcode image into same folder as the uploaded photos
-  //       const qrCodeUrl = await uploadQRCodeToFireBase(qrcode, hash)
+        // Sends qr code url to email, hash is used to add qrcode image into same folder as the uploaded photos
+        const qrCodeUrl = await uploadQRCodeToFireBase(qrcode, hash)
 
-  //       console.log('QRCODE URL---->>>>', qrCodeUrl)
-  //       console.log('THIS IS THE HASH ------------------------', hash)
+        console.log('QRCODE URL---->>>>', qrCodeUrl)
+        console.log('THIS IS THE HASH ------------------------', hash)
 
-  //       // looks for user and updates paid to true
-  //       const user = await User.findOneAndUpdate(
-  //         { hash: hash }, // Query to find the user
-  //         { 
-  //           paid: true,   // Update the paid field
-  //           qrCode: qrcode // Update the qrCode field
-  //         },
-  //         { new: true } // Return the updated document
-  //       );
-  //       console.log(user)
+        // looks for user and updates paid to true
+        const user = await User.findOneAndUpdate(
+          { hash: hash }, // Query to find the user
+          { 
+            paid: true,   // Update the paid field
+            qrCode: qrcode // Update the qrCode field
+          },
+          { new: true } // Return the updated document
+        );
+        console.log(user)
 
-  //       // Email Message configuration
-  //       const config = {
-  //         to: customerEmail,  
-  //         subject: `Your QR Code and Details`,
-  //         text: `Thank you for your purchase! Here is your QR code. If image is not available copy and paste this link: ${url}`,
-  //         html: `
-  //           <h1>Detalhes da sua Compra</h1>
-  //           <p>Obrigado, ${name}, por usar nossos serviços!</p>
-  //           <p>Here are your details:</p>
-  //           <ul>
-  //             <li><strong>Date:</strong> ${date}</li>
-  //             <li><strong>Time:</strong> ${time}</li>
-  //           </ul>
-  //           <p>Here is your QR code:</p>
-  //           <img src="${qrCodeUrl}" alt="QR Code" />
-  //           <p>Caso a image do QrCode não esteja aparecendo, você pode accessar com o link abaixo:</p>
-  //           <p>${siteUrl}/${url}</p>
-  //         `
-  //       }
-  //       // Send email with QR code and details 
-  //       await sendMail(config);
+        // Email Message configuration
+        const config = {
+          to: customerEmail,  
+          subject: `Your QR Code and Details`,
+          text: `Thank you for your purchase! Here is your QR code. If image is not available copy and paste this link: ${url}`,
+          html: `
+            <h1>Detalhes da sua Compra</h1>
+            <p>Obrigado, ${name}, por usar nossos serviços!</p>
+            <p>Here are your details:</p>
+            <ul>
+              <li><strong>Date:</strong> ${date}</li>
+              <li><strong>Time:</strong> ${time}</li>
+            </ul>
+            <p>Here is your QR code:</p>
+            <img src="${qrCodeUrl}" alt="QR Code" />
+            <p>Caso a image do QrCode não esteja aparecendo, você pode accessar com o link abaixo:</p>
+            <p>${siteUrl}/${url}</p>
+          `
+        }
+        // Send email with QR code and details 
+        await sendMail(config);
 
-  //     } catch (error) {
-  //       console.error('Error processing webhook:', error);
-  //       return NextResponse.json({ error: 'Failed to process the payment and upload photos.'}, { status: 500 });
-  //     }
-  //   }
-  // }
+      } catch (error) {
+        console.error('Error processing webhook:', error);
+        return NextResponse.json({ error: 'Failed to process the payment and upload photos.'}, { status: 500 });
+      }
+    }
+  }
 
-  // console.log('THIS IS EVENT TYPE AT THE END', event.type)
+  console.log('THIS IS EVENT TYPE AT THE END', event.type)
 
   return NextResponse.json({ success: true }, { status: 200 });
 
