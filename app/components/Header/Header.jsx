2a2a7aa023b1c0f379748e778589e6c3eddc15c9
marceloaraujodev <codetrsh@gmail.com>
2024-10-09 19:14:46 -0400

    icon

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import c from './Header.module.css'
 export default function Header() {
   return (
     <header className={c.header}>
-    <a className={c.link} href='https://www.qrcodelove.com'><img className={c.logoQr} src='/logoQRcodeHeart.png' alt='logo' />QR CODE LOVE</a>
+    <a className={c.link} href='https://www.qrcodelove.com'><img className={c.logoQr} src='/neonlogo-clean2.png' alt='logo' />QR CODE LOVE</a>
   </header>
   )
 }
