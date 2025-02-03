a75d55abdc1522994ce4510b3101dd994be9f832
marceloaraujodev <codetrsh@gmail.com>
2025-02-03 01:17:06 -0300

    cron wip

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,15 +1,22 @@
 import { NextResponse } from 'next/server';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export async function GET(request) {
-  console.log(process.env.CRON_SECRET);
+  const authHeader = request.headers.get('Authorization')?.trim();
+  const cronSecret = process.env.CRON_SECRET?.trim();
+  console.log(`Auth Header: "${authHeader}"`);
+  console.log(`Env Secret: "Bearer ${process.env.CRON_SECRET}"`);
+  console.log('cronSecret: ', cronSecret);
   // Verify cron secret
-  const authHeader = request.headers.get('Authorization');
-  console.log('this is authHeader:', authHeader);
+  // const authHeader = request.headers.get('Authorization');
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new NextResponse('Unauthorized', { status: 401 });
   }
 
   console.log('Cron job executed successfully');
+  
   
   // Add your cron logic here
   // Example: database cleanup, data sync, etc.
